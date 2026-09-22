1cf498240e5ef3ef47c021ee1b86a2de5032b250
magicdoes <braiden.thomas08@icloud.com>
2026-09-22 20:56:46 +0100

    Delete build.sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-mkdir -p build
-mvn -q install:install-file -Dfile=baseline/MagicSMP-current.jar -DgroupId=com.bx -DartifactId=magicsmp-current -Dversion=1.0 -Dpackaging=jar
-mvn -q -f guard/pom.xml clean package
-mvn -q -f patcher/pom.xml clean package
-java -jar patcher/target/patcher.jar baseline/MagicSMP-current.jar guard/target/classes build/MagicSMP.jar
